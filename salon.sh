9067efc40720fd6690c1e48fb31b6eb5842585ec
silum-gh <28903417+silum-gh@users.noreply.github.com>
2024-02-12 12:39:06 -0500

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -3,9 +3,13 @@
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\n\n~~~~~ MY SALON ~~~~~\n\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
 
 MAIN_MENU() {
-  echo -e "Welcome to My Salon, how can I help you?\n"
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
 
   # get services
   SERVICES_AVAILABLE=$($PSQL "SELECT * FROM services ORDER BY service_id;")
@@ -29,14 +33,14 @@ MAIN_MENU() {
   else
    
     SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id='$SERVICE_ID_SELECTED';")
-    echo $SERVICE_NAME
+
     # get phone number
     echo -e "\nWhat is your phone number?"
     read CUSTOMER_PHONE
-    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE';")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE';")
 
     # if new customer
-    if [[ -z $CUSTOMER_NAME ]]
+    if [[ -z $CUSTOMER_ID ]]
     then
       # get new customer name
       echo -e "\nWhat's your name?"
@@ -44,10 +48,10 @@ MAIN_MENU() {
 
       # add new customer
       INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE');")
-
     fi
     
-    # get customer id
+    # get customer name and ID
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE';")
     CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE';")
 
     # ask for time
@@ -58,18 +62,10 @@ MAIN_MENU() {
     ADD_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID, '$SERVICE_TIME');")
 
     # booking message/redirect/exit
-    echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
-    echo -e "\nCan I help you with anything else?\nPress 1 to book another appointment.\nPress any other key to exit."
-    read MENU_SELECTION
-    case $MENU_SELECTION in
-      1) MAIN_MENU ;;
-      *) EXIT ;;
-    esac
+    echo -e "I have put you down for a$SERVICE_NAME at $SERVICE_TIME,$CUSTOMER_NAME."
+    
   fi
 }
 
-EXIT() {
-  echo -e "\nThank you for stopping in."
-}
 
 MAIN_MENU
